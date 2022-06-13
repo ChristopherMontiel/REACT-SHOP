308bc512be9610ad72058d4690abe81d926ac1f8
Christopher Montiel <christopher.montiel.m@gmail.com>
2022-06-13 11:24:15 -0400

    [FIX] Se reemplaza routerBrowser por HashBrowser

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route }  from 'react-router-dom';
+import { HashRouter, Routes, Route }  from 'react-router-dom';
 import Layout from '@containers/Layout';
 import ChangePassword from '@pages/ChangePassword';
 import RecoveryPassword from '@pages/RecoveryPassword';
@@ -15,10 +15,10 @@ import '@styles/global.css';
 
 const App = () => {
   const initialState = useInitialState();
-  
+
   return (
     <AppContext.Provider value={ initialState } >
-      <BrowserRouter>
+      <HashRouter>
         <Layout>
           <Routes>
             <Route path='/' element = {<Home />} />
@@ -32,7 +32,7 @@ const App = () => {
             <Route path='*' element = {<NotFound/>} />
           </Routes>
         </Layout>
-      </BrowserRouter>
+      </HashRouter>
     </AppContext.Provider>
   );
 }
